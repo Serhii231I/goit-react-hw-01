3f54088a71e2d470e07ea28220c53f9644767615
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2024-12-09 21:03:47 +0100

    add css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,9 +1,9 @@
-
 import FriendListItem from '../FriendListItem/FriendListItem';
+import styles from './FriendList.module.css';
 const FriendList = ({ friends }) => {
 
     return (
-        <ul>        
+        <ul className={styles.friendList}>        
             {friends.map(({ avatar, name, isOnline, id }) => (
                 <FriendListItem key={id} avatar={avatar} name={name} isOnline={isOnline} />
             ))}    
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,9 +1,11 @@
+import styles from './FriendListItem.module.css';
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li>
-      <img src={avatar} alt={`${name}'s avatar`} width="48" />
-      <p>{name}</p>
-      <p>{isOnline ? 'Online' : 'Offline'}</p>
+    <li className={styles.friendItem}>
+      <span className={isOnline ? styles.online : styles.offline}></span>
+      <img src={avatar} alt={`${name}'s avatar`} width="80" />
+      <p className={styles.name}>{name}</p>
+      <p className={`${styles.status} ${isOnline ? styles.online : styles.offline}`}>{isOnline ? 'Online' : 'Offline'}</p>
     </li>
   );
 };
